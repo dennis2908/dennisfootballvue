17a85270242a1046c46b48d2caa5a1f54fa7beb9
unknown <manullang_d@yahoo.com>
2021-07-26 15:04:56 +0700

    dennis is cool

---
+++ src/views/Modal/ModalPlayer.vue
@@ -0,0 +1,34 @@
+<template>
+<CModal
+      :title="storeDM.state.titleModal"
+      color="primary"
+      :show.sync="storeDM.state.warningModal"
+    >
+	<FormsPlayer />
+   	  <template #footer>
+                <CButton @click="storeDM.commit('changeWarningModal',{modalState:false})" color="secondary"><CIcon name="cil-x-circle"/> Cancel</CButton>
+      </template>	
+			
+    </CModal>
+</template>
+<script>
+	import { storeDM } from '@/views/../redux/storePlayer'
+	import FormsPlayer from '@/views/players/FormsPlayer'
+	export default {
+	  name: 'ModalPlayer',
+	  components: {FormsPlayer},
+	  data(){
+	   return {
+		  storeDM:storeDM
+	      
+	   }
+	  },
+	  methods: {
+	    submit(){
+		  
+		
+		} 
+	  }
+	  
+	}
+</script>
